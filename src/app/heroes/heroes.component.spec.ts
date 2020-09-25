6bf6f6127d47154b478bb307cdc228f456599b49
Luis Francisco Angulo Andrade <luis.angulo@sonorasoft.com>
2020-09-24 17:11:45 -0700

    Test mocking components

---
+++ src/app/heroes/heroes.component.spec.ts
@@ -0,0 +1,29 @@
+import { of } from 'rxjs';
+import { Hero } from '../hero';
+import { HeroesComponent } from './heroes.component';
+
+describe('HeroesComponent', () => {
+  let component: HeroesComponent;
+  let HEROES: Hero[];
+  let mockHeroSvc;
+
+  beforeEach(() => {
+    HEROES = [
+      { id: 1, name: 'SpiderDude', strength: 8 },
+      { id: 2, name: 'Wonderful woman', strength: 24 },
+      { id: 3, name: 'SuperDude', strength: 55 },
+    ];
+    // mocking the svc with jasmine requires the methods to mock as strings
+    mockHeroSvc = jasmine.createSpyObj(['getHeroes', 'addHero', 'deleteHero']);
+    component = new HeroesComponent(mockHeroSvc);
+  });
+
+  it('Should remove the correct hero from its list', () => {
+    mockHeroSvc.deleteHero.and.returnValue(of(true));
+    component.heroes = HEROES;
+
+    component.delete(HEROES[2]);
+
+    expect(component.heroes.length).toBe(2);
+  });
+});
